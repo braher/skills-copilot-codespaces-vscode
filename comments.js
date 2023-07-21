79d2d565f611b69f33238b1c9d3c5d182fa0c461
Brayan Hernandez <bryandhf@gmail.com>
2023-07-21 01:08:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,3 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
+
+router.post('/posts/:id/comments', isLoggedIn, commentsCtrl.create);
+router.delete('/comments/:id', isLoggedIn, commentsCtrl.delete);
+router.put('/comments/:id', isLoggedIn, commentsCtrl.update);
+router.get('/comments/:id/edit', isLoggedIn, commentsCtrl.edit);
